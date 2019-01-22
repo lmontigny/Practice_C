3375ab6fd5fafdaf941b4e366f665751ab7570ca
Laurent Montigny <lmontigny@users.noreply.github.com>
2019-01-22 23:28:22 +0100

    Update pointer_casting.c

--- pointer_casting.c
+++ pointer_casting.c
@@ -1,7 +1,14 @@
-double PI = 3.1415927;
- 
-char* pc = (char*) &PI;          // (1) safe
-char byte0 = *pc;                // (2) safe
- 
-double* pd = (double*) &byte0;   // (3) unsafe
-double d = *pd;                  // (4) undefined behavior
+#include <stdio.h>
+
+int main(void)
+{
+ double PI = 3.1415927;
+
+ char* pc = (char*) &PI;          // (1) safe
+ char byte0 = *pc;                // (2) safe
+
+ double* pd = (double*) &byte0;   // (3) unsafe
+ double d = *pd;                  // (4) undefined behavior
+
+ return 0;
+}

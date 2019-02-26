f773f7f35aeedd111922058c7f40839d58100e38
Laurent Montigny <lmontigny@users.noreply.github.com>
2019-02-26 09:37:14 +0100

    Update pointer_casting.c

--- pointer_casting.c
+++ pointer_casting.c
@@ -12,3 +12,24 @@ int main(void)
 
  return 0;
 }
+
+
+
+/*
+From Stack overflow:
+
+(int *)p=&l;
+The line above doesn't work, because as soon as you cast p to (int*), 
+the result is an anonymous temporary object, which is an rvalue and not an lvalue; 
+consquently, the result cannot receive the assignment, and even if the language did allow it, 
+you'd be assigning to a temporary casted copy of p, not to the original p.
+
+p=&((char) l);
+The line above does not work for a similar reason; 
+the result of (char) l is a temporary object that is a copy of l casted to the type char. 
+Consequently, because it is temporary, you cannot take its address.
+
+Insead, you can use:
+p = (char*) &l
+
+*/

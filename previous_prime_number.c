37cb4eb1e24d6efbd327c711fbed3c350c2adb04
Laurent Montigny <lmontigny@users.noreply.github.com>
2019-02-27 13:54:59 +0100

    Create previous_prime_number.c

---
+++ previous_prime_number.c
@@ -0,0 +1,27 @@
+
+int main()
+{
+    int num1, ctr = 0;
+    num1 = 99;
+
+    for (int n = num1 - 1; n >= 1; n--) 
+    {
+        for (int m = 2; m < n; m++) 
+        {
+            if (n % m == 0)
+                ctr++;
+        }
+        if (ctr == 0) 
+        {
+            if (n == 1) 
+            {
+                printf("no prime number less than 2\n");
+                break;
+            }
+            printf(" %d is the last prime number before %d \n ", n, num1);
+            break;
+        }
+        ctr = 0;
+    }
+    return 0;
+}
